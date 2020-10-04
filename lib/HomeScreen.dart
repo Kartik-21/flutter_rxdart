caecc9c41cf6b2bf26243172e084f1c8c3f64ff0
Kartik-21 <kartikjain2926@gmail.com>
2020-10-04 17:40:28 +0530

    Initial commit

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_rxdart/blocs/HomeScreenBloc.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  double _hight, _width, _blockOfHight, _blockOfWidth;
+
+  @override
+  Widget build(BuildContext context) {
+    _hight = _hight ?? MediaQuery.of(context).size.height;
+    _width = _width ?? MediaQuery.of(context).size.width;
+    _blockOfWidth = _blockOfWidth ?? (_width / 100);
+    _blockOfHight = _blockOfHight ?? (_hight / 100);
+
+    final bloc = HomeScreenBloc();
+
+    return Container(
+      height: _hight,
+      width: _width,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("RxDart"),
+          elevation: 6.0,
+          centerTitle: true,
+        ),
+        body: Container(
+          padding: EdgeInsets.symmetric(horizontal: _blockOfHight * 1),
+          child: ListView(
+            children: [
+              SizedBox(
+                height: _blockOfHight * 15,
+              ),
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: _blockOfHight * 1.5),
+                  child: StreamBuilder<String>(
+                    stream: bloc.email,
+                    builder: (context, snapshot) {
+                      return TextField(
+                        onChanged: (s) => bloc.emailChanged.add(s),
+                        decoration: InputDecoration(
+                            labelText: "Enter Email",
+                            errorText: snapshot.error,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      );
+                    },
+                  )),
+              Padding(
+                  padding: EdgeInsets.symmetric(vertical: _blockOfHight * 1.5),
+                  child: StreamBuilder<String>(
+                    stream: bloc.password,
+                    builder: (context, snapshot) {
+                      return TextField(
+                        onChanged: (s) => bloc.passwordChanged.add(s),
+                        decoration: InputDecoration(
+                            labelText: "Enter Password",
+                            errorText: snapshot.error,
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      );
+                    },
+                  )),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: _blockOfHight * 2),
+                child: SizedBox(
+                  height: _blockOfHight * 8,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: StreamBuilder<bool>(
+                      stream: bloc.submitCheck,
+                      builder: (context, snapshot) {
+                        return RaisedButton(
+                          onPressed: snapshot.hasData
+                              ? () {
+                                  debugPrint("submit");
+                                }
+                              : null,
+                          elevation: 6.0,
+                          color: Theme.of(context).accentColor,
+                          child: Text(
+                            "Submit",
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/blocs/HomeScreenBloc.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:rxdart/rxdart.dart';
+
+class HomeScreenBloc extends Object with Validator {
+  final _emailController = PublishSubject<String>();
+  final _passwordControlller = PublishSubject<String>();
+
+  StreamSink<String> get emailChanged => _emailController.sink;
+
+  StreamSink<String> get passwordChanged => _passwordControlller.sink;
+
+  Stream<String> get email => _emailController.stream.transform(emailvalidator);
+
+  Stream<String> get password =>
+      _passwordControlller.stream.transform(emailvalidator);
+
+  Stream<bool> get submitCheck =>
+      Rx.combineLatest2(email, password, (e, p) => true);
+
+  void dispose() {
+    _emailController?.close();
+    _passwordControlller?.close();
+  }
+}
+
+mixin Validator {
+  var emailvalidator = StreamTransformer<String, String>.fromHandlers(
+    handleData: (data, sink) {
+      if (data.length > 6) {
+        sink.add(data);
+      } else {
+        sink.addError("enter lenght >6");
+      }
+    },
+  );
+}
